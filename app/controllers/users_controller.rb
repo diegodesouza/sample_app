d645fb84ecd170bbba0476104c13e6c3e2c277c9
diegodesouza <diegotrus7@gmail.com>
2016-01-14 13:54:49 -0500

    Add profile page, gravatar/gravatar helper.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+
+  def show
+  end
+
   def new
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
