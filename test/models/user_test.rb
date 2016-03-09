68d02d8a8b328d17b4ae0e8a39fecd679e065b68
diegodesouza <diegotrus7@gmail.com>
2016-03-09 13:14:14 -0500

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -94,4 +94,25 @@ class UserTest < ActiveSupport::TestCase
     diego.unfollow(archer)
     assert_not diego.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    diego = users(:diego)
+    archer = users(:archer)
+    lana = users(:lana)
+
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert diego.feed.include?(post_following)
+    end
+
+    # Posts from self
+    diego.microposts.each do |post_self|
+      assert diego.feed.include?(post_self)
+    end
+
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not diego.feed.include?(post_unfollowed)
+    end
+  end
 end
